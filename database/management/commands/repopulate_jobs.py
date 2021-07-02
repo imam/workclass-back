e69e266e8a6ddfacace282fb675d3a4c1878ca18
Imam Assidiqqi <assidiqqi.imam@gmail.com>
2021-07-02 17:52:28 +0700

    Typo repopulate jobs

--- database/management/commands/repopulate_jobs.py
+++ database/management/commands/repopulate_jobs.py
@@ -2,7 +2,7 @@ from django.core.management.base import BaseCommand
 from database.models import Job
 
 class Command(BaseCommand):
-    help = 'Repolpulate Jobs'
+    help = 'Repopulate Jobs'
 
     def handle(self, *args, **kwargs):
         for job in Job.objects.all():
